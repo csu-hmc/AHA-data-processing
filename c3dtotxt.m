9ce10e89bac3a7a6ffc88872b23827745ea11d4f
Ton van den Bogert <a.vandenbogert@csuohio.edu>
2021-10-07 18:38:29 -0400

    c3dtotxt.m: now handles Par8 PRE correctly

--- c3dtotxt.m
+++ c3dtotxt.m
@@ -29,7 +29,7 @@ function result = c3dtotxt(c3d_filename, txt_filename)
             fprintf('Your computer name is: %s\n', computer);
             fprintf('Please configure c3dtotxt.m for your computer.\n');
         end
-        trial = 'Par1_POST\Mocap0001';
+        trial = 'Par10_POST\Mocap0001';
         txt_filename = [datapath trial '.txt'];
         c3d_filename = [datapath trial '.c3d'];
     end
@@ -113,11 +113,11 @@ function result = c3dtotxt(c3d_filename, txt_filename)
         end
     end
     
-    % If there are no markers in the TXT file (this happened in Par12_POST),
+    % If there is not a full markerset in the TXT file,
     % we add the standard 47-marker set and missing data for all markers and 
     % all frames. This allows processing to continue, and C3D marker data will be used
     % We also do this when the TXT file has unlabeled markers, 47 names all containing the letter M
-    if numel(txtMarkerNames) == 0 || sum(contains(txtMarkerNames,'M')) == 47
+    if numel(txtMarkerNames) < 47 || sum(contains(txtMarkerNames,'M')) == 47
         txtMarkerNames = {'LHEAD' 'THEAD' 'RHEAD' 'FHEAD' 'C7'   'T10'  'SACR' 'NAVE'  'XYPH'  'STRN'  ...
                           'BBAC'  'LSHO'  'LDELT' 'LLEE'  'LMEE' 'LFRM' 'LMW'  'LLW'   'LFIN'  'RSHO'  ...
                           'RDELT' 'RLEE'  'RMEE'  'RFRM'  'RMW'  'RLW'  'RFIN' 'LASIS' 'RASIS' 'LPSIS' ...
